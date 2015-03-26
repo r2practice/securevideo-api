6aaa0423e56c9bffee7d8f9dd0bfd51c67876804
Maxim Pechnikov <parallel588@gmail.com>
2015-03-26 12:35:25 +0300

    added test for users

--- spec/securevideo/api/user_spec.rb
+++ spec/securevideo/api/user_spec.rb
@@ -1,19 +1,58 @@
 require 'spec_helper'
 
-describe Securevideo::Api::User do
+describe Securevideo::Api::User, vcr: true do
 
   describe 'GET get users' do
+    it 'should return user list' do
+      expect(Securevideo::Api::User.all.fetch).to eq([Securevideo::Api::User.new('FullName' =>"Test User",
+                                                                                 'EmailAddress' => "test@test.com",
+                                                                                 'SmsNumber' => "",
+                                                                                 'DefaultResolution' => "high",
+                                                                                 'TimeZoneWindowsId' => "Pacific Standard Time",
+                                                                                 'HelpNumber' => "4084062331",
+                                                                                 'VideoId' => "securevideo+sv000001216323790188",
+                                                                                 'SystemUserId' => "12163")])
+    end
   end
 
   describe 'GET get user' do
+    it 'should return user' do
+    expect(Securevideo::Api::User.find(12163)).to eq(Securevideo::Api::User.new('FullName' =>"Test User",
+                                                                               'EmailAddress' => "test@test.com",
+                                                                               'SmsNumber' => "",
+                                                                               'DefaultResolution' => "high",
+                                                                               'TimeZoneWindowsId' => "Pacific Standard Time",
+                                                                               'HelpNumber' => "4084062331",
+                                                                               'VideoId' => "securevideo+sv000001216323790188",
+                                                                               'SystemUserId' => "12163"))
+    end
   end
 
   describe 'POST create user' do
+    it 'should create user' do
+      user = Securevideo::Api::User.new(FullName: 'Test User',
+                                        TimeZoneWindowsId: "Pacific Standard Time",
+                                        EmailAddress: 'testuser@test.com',
+                                        HelpNumber: '4084062331')
+      user.save
+      expect(user.id).to be_present
+    end
   end
 
   describe 'PUT update user' do
+    it 'should update user' do
+      user = Securevideo::Api::User.find 12283
+      user.FullName = 'testUserName'
+      user.save
+      expect(Securevideo::Api::User.find(12283).FullName).to eq('testUserName')
+    end
   end
 
   describe 'DELETE delete user' do
+    it 'delete user' do
+      user = Securevideo::Api::User.find 12283
+      user.destroy
+      expect(Securevideo::Api::User.find(12283)).to be_nil
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,12 +6,20 @@ require 'webmock/rspec'
 require 'timecop'
 
 require 'vcr'
+SKEY = 'xxxx'
+SSECRET = 'xxxx'
+Securevideo::Api.configure(SKEY, SSECRET)
+
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.allow_http_connections_when_no_cassette = false
+  config.default_cassette_options = { record: :once,
+                                      match_requests_on: [:method, :uri]
+                                    }
+
 end
 
 RSpec.configure do |config|
